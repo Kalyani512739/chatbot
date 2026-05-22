c1a12599264f52bbeb5daac733a2b9e54c68d465
Kalyani512739 <kaly32184@gmail.com>
2026-05-22 11:31:49 +0530

    supabase

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ app = Flask(__name__)
 
 app.secret_key = "supersecretkey"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///users.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 
 db = SQLAlchemy(app)
